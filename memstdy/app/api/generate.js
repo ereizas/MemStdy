1b181078e9fe22c93705c72b3f63387411b626c6
Eric Reizas <tuo17224@temple.edu>
2024-08-14 09:13:01 -0400

    Created function to generate flashcards in POST request

--- memstdy/app/api/generate.js
+++ memstdy/app/api/generate.js
@@ -13,3 +13,28 @@ You should return in the following JSON format:
   ]
 }
 `
+
+export async function POST(req){
+    const data = await req.text();
+
+    const completion = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.LLAMA_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "meta-llama/llama-3.1-8b-instruct:free",
+            messages: [
+                { role: 'system', content: systemPrompt },
+                { role: 'user', content: data },
+            ],
+            top_p: 1,
+            temperature: 1,
+            repetition_penalty: 1,
+            response_format: { type: 'json_object' },
+        })
+    })
+    const flashcards = JSON.parse(completion.choices[0].message.content)
+    return NextResponse.json(flashcards.flashcards)
+}
