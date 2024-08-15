864923b3550729929ccaa07b26a03edcb0397bdb
JimmyJiang <tul70511@temple.edu>
2024-08-14 21:21:55 -0400

    added etching Flashcard Sets

--- memstdy/app/generate/page.js
+++ memstdy/app/generate/page.js
@@ -28,6 +28,24 @@ export default function Flashcard() {
   const router = useRouter();
 
   // ... (rest of the component)
+
+    // Fetching Flashcard Sets
+    useEffect(() => {
+      async function getFlashcards() {
+        if (!user) return;
+        const docRef = doc(collection(db, 'users'), user.id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const collections = docSnap.data().flashcards || [];
+          setFlashcards(collections);
+        } else {
+          await setDoc(docRef, { flashcards: [] });
+        }
+      }
+      getFlashcards();
+    }, [user]);
+
+    
 }
 
 
